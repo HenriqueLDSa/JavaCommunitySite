b1d70c9b8a0ca081aad3627203ee42bf7621c1e9
HenriqueLDSa <rique.ldsa@gmail.com>
2025-07-13 18:40:02 -0500

    added first and last name to user schema

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -3,6 +3,8 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 CREATE TABLE "user" (
             id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
             username TEXT UNIQUE NOT NULL,
+            first_name TEXT UNIQUE NOT NULL,
+            last_name TEXT UNIQUE NOT NULL,
             email TEXT UNIQUE NOT NULL,
             password_hash TEXT NOT NULL,
             created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
