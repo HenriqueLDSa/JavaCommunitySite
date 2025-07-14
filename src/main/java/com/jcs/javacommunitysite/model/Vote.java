e36de9c96bd59d5e3495ebceadf98b80ccf38d8d
HenriqueLDSa <rique.ldsa@gmail.com>
2025-07-13 20:18:19 -0500

    update vote entity

--- src/main/java/com/jcs/javacommunitysite/model/Vote.java
+++ src/main/java/com/jcs/javacommunitysite/model/Vote.java
@@ -5,9 +5,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.ColumnDefault;
-import org.hibernate.annotations.OnDelete;
-import org.hibernate.annotations.OnDeleteAction;
 
 import java.util.UUID;
 
@@ -18,19 +15,18 @@ import java.util.UUID;
 @Builder
 @Table(uniqueConstraints = @UniqueConstraint(columnNames = {"user_id", "post_id"}))
 public class Vote {
-
     @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
+    @GeneratedValue
     private UUID id;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(optional = false)
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(optional = false)
     @JoinColumn(name = "post_id", nullable = false)
     private Post post;
 
     @Column(name = "vote_type", nullable = false)
-    private Short voteType; // should be -1 or 1, enforce in service logic
+    private short voteType;  // -1 or 1
 }
