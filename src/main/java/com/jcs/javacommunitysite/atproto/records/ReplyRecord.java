9f5b67689c6df1ae45317e2d073bbfc6fb0d9b90
fudgeu <patrickzambonie@gmail.com>
2025-10-17 22:47:39 -0400

    Forgot to include 1 file

--- src/main/java/com/jcs/javacommunitysite/atproto/records/ReplyRecord.java
+++ src/main/java/com/jcs/javacommunitysite/atproto/records/ReplyRecord.java
@@ -28,7 +28,7 @@ public class ReplyRecord extends AtprotoRecord {
     public ReplyRecord(String content, AtUri root) {
         this.content = content;
         this.createdAt = Instant.now();
-        this.updatedAt = Instant.now(); // TODO remove
+        this.updatedAt = Instant.now();
         this.root = root;
     }
 
